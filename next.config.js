3de0321e59e67f97535c698dc1274e45ae1c05d4
Thomas Greco <6646552+tgrecojs@users.noreply.github.com>
2023-05-13 05:58:06 -0400

    fix: updated export script to outport files to the 'export' directory

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
 const withNextra = require('nextra')({
   theme: 'nextra-theme-blog',
   themeConfig: './theme.config.js',
-})
+});
 
 module.exports = {
   ...withNextra(),
-  output: 'export',
   images: {
     unoptimized: true,
   },
